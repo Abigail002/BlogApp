924af2a234ebf0104a012bf3b8560fea5e43027d
Abigail002 <abigailameogno98@gmail.com>
2023-09-25 19:33:10 +0000

    Add of logging out

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -6,7 +6,8 @@
         </a>
 
         <!-- Navbar toggler button -->
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
+            aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
             <div class="navbar-toggler-icon">
                 <span></span>
                 <span></span>
@@ -16,10 +17,12 @@
 
         <!-- Navbar content -->
         <div class="collapse navbar-collapse" id="navbarContent">
-            <div class="navbar-content-inner ms-lg-auto d-flex flex-column flex-lg-row align-lg-center gap-4 gap-lg-10 p-2 p-lg-0">
+            <div
+                class="navbar-content-inner ms-lg-auto d-flex flex-column flex-lg-row align-lg-center gap-4 gap-lg-10 p-2 p-lg-0">
                 <ul class="navbar-nav gap-lg-2 gap-xl-5">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle " href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
                             Home
                         </a>
                         <ul class="dropdown-menu">
@@ -34,7 +37,16 @@
                     </li>
                 </ul>
                 <div class="">
-                    <x-cta-button name="" url="/login">Get started</x-cta-button>
+                    @auth
+                    <form action="{{ url('/logout') }}" method="POST">
+                        @csrf
+                        <button class="btn btn-outline-primary-dark">Deconnexion</button>
+                    </form>
+                    @endauth
+
+                    @guest
+                        <x-cta-button name="" url="/login">Get started</x-cta-button>
+                    @endguest
                 </div>
             </div>
         </div>
