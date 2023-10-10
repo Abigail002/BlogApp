d3aef0acf7c2a336290b160e83feeb780a713b04
Abigail002 <abigailameogno98@gmail.com>
2023-10-10 12:04:36 +0000

    Removal of PostController protection by auth middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
      * Display a listing of the resource.
      */
    public function __construct() {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     public function index()
